b00873d0b6244df23ebc46e272149f0e58b7fd74
Sergio-Nav <sergio.navascastro@gmail.com>
2024-09-30 09:38:15 +0200

    Suma2

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,10 +3,26 @@
         let num1 = prompt("Introduce el primer numero");
         let num2 = prompt("Introduce el segundo numero");
 
+        Suma(num1,num2);
+        Resta(num1,num2);
         Mult(num1,num2);
+        Dividir(num1,num2);
+    }
+    function Suma(num1,num2){
+        //Suma modificada
+        let res = num1 + num2;
+        console.log("Suma: "+ res);
+    }
+    function Resta(num1,num2){
+        let res = num1 - num2;
+        console.log("Resta: "+ res);
     }
     function Mult(num1,num2){
         let res = num1 * num2;
         console.log("Multiplicación: "+ res);
+    }
+    function Dividir(num1,num2){
+        let res = num1 / num2;
+        console.log("Division: "+ res);
     }
 }
