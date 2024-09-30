3d96b4c4b21fec9d61d1862c76e32eb9785b6dfd
Sergio-Nav <sergio.navascastro@gmail.com>
2024-09-30 09:01:57 +0200

    Resta

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,10 +3,10 @@
         let num1 = prompt("Introduce el primer numero");
         let num2 = prompt("Introduce el segundo numero");
 
-        Suma(num1,num2);
+        Resta(num1,num2);
     }
-    function Suma(num1,num2){
-        let res = num1 + num2;
-        console.log("Suma: "+ res);
+    function Resta(num1,num2){
+        let res = num1 - num2;
+        console.log("Resta: "+ res);
     }
 }
